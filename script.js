d24de8d33ce9033d0e40dc8df6743a52e28f72db
Olaitan <abiodunfatunbi4@gmail.com>
2022-12-24 21:01:07 +0100

    Updated the Css

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const menuBtn=document.querySelector(".menu-btn")
 let openBtn= document.querySelector(".open-icon")
 let closeBtn=document.querySelector(".close-icon")
 let menuItem=document.querySelector(".menu-list")
+let headerHeight=document.querySelector(".header")
 menuBtn.addEventListener("click",()=>{
     if(menuBtn.classList.contains("active")){
         menuBtn.classList.toggle("active")
         openBtn.style.display="none"
         closeBtn.style.display="block"
         menuItem.style.display="block"
+        headerHeight.style.height="123vh"
         
     }
     else{
@@ -15,5 +17,6 @@ menuBtn.addEventListener("click",()=>{
         openBtn.style.display="block"
         closeBtn.style.display="none"
         menuItem.style.display="none"
+        headerHeight.style.height="102.2vh"
     }
 })
